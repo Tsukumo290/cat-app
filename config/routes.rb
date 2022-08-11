0f61576b7da6808ac9f828c398a0fb202e010eea
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-11 10:03:06 +0900

    即時反映の実装変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :posts do
     resource :favorites, only: [:create, :destroy]
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
 end
