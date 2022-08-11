0240443755f6788b3bc445af4d741093feaed437
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-11 12:30:34 +0900

    コメント削除の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(params[:post_id])
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
