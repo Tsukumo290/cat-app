72ff79b6e567da4d3389eb11ddefc8dadae908fa
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-12 16:17:22 +0900

    フォロー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,26 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   mount_uploader :image, ImageUploader
 
   validates :nickname, presence: true
   validates :profile, length: { maximum: 250 }
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
 end
